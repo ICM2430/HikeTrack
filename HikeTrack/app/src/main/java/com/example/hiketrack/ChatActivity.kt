f6663b24eb1226006e5942b4ab9adf17c25e4450
CryoWarrior <diegoalejandrojararojas@gmail.com>
2024-08-27 15:56:55 -0500

    Agregar actividades Chat y Emergencia

---
+++ HikeTrack/app/src/main/java/com/example/hiketrack/ChatActivity.kt
@@ -0,0 +1,17 @@
+package com.example.hiketrack
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.hiketrack.databinding.ActivityChatBinding
+
+class ChatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChatBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
