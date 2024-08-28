f3fbe94107832820a54c8a88d9e5d0492fcdc0b9
CryoWarrior <diegoalejandrojararojas@gmail.com>
2024-08-27 23:04:19 -0500

    Reparar Manifest, Colores y Strings

--- HikeTrack/app/src/main/java/com/example/hiketrack/LoginActivity.kt
+++ HikeTrack/app/src/main/java/com/example/hiketrack/LoginActivity.kt
@@ -5,16 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.hiketrack.databinding.ActivityLoginBinding
+import android.content.Intent
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.UnderlineSpan
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // Subrayar el TextView de "Crear cuenta"
+        val createAccountText = SpannableString("Crear cuenta")
+        createAccountText.setSpan(UnderlineSpan(), 0, createAccountText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.createAccountTextView.text = createAccountText
+
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, FeedActivity::class.java) // Define el intent para FeedActivity
+            startActivity(intent)
         }
+
+        // Manejar clics para el texto de crear cuenta
+        binding.createAccountTextView.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java) // Define el intent para RegisterActivity
+            startActivity(intent)
+        }
+
     }
 }
