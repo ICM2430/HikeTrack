528081df9e3ae37441af92e443b511686a0685f6
CryoWarrior <diegoalejandrojararojas@gmail.com>
2024-08-28 08:43:02 -0500

    Consistencia de pantallas

--- HikeTrack/app/src/main/java/com/example/hiketrack/EstadisticasActivity.kt
+++ HikeTrack/app/src/main/java/com/example/hiketrack/EstadisticasActivity.kt
@@ -1,20 +1,38 @@
 package com.example.hiketrack
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.hiketrack.databinding.ActivityEstadisticasBinding
 
 class EstadisticasActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEstadisticasBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_estadisticas)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityEstadisticasBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.home.setOnClickListener {
+            val intent =  Intent(this,FeedActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.seleccionarRecorrido.setOnClickListener {
+            val intent =  Intent(this,SeleccionarRecorrido::class.java)
+            startActivity(intent)
+        }
+
+        binding.botonRetos.setOnClickListener {
+            val intent =  Intent(this,RetosActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.contactos.setOnClickListener {
+            val intent =  Intent(this,Contactos::class.java)
+            startActivity(intent)
         }
     }
 }
--- HikeTrack/app/src/main/java/com/example/hiketrack/FeedActivity.kt
+++ HikeTrack/app/src/main/java/com/example/hiketrack/FeedActivity.kt
@@ -2,10 +2,7 @@ package com.example.hiketrack
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.hiketrack.databinding.ActivityFeedBinding
 
 class FeedActivity : AppCompatActivity() {
@@ -27,37 +24,25 @@ class FeedActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        // Set up bottom navigation item selection
-        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_home -> {
-                    val intent = Intent(this, FeedActivity::class.java)
-                    startActivity(intent)
-                    true
-                }
-                R.id.navigation_stats -> {
-                    val intent = Intent(this, EstadisticasActivity::class.java)
-                    startActivity(intent)
-                    true
-                }
-                R.id.navigation_challenges -> {
-                    val intent = Intent(this, SeleccionarRecorrido::class.java)
-                    startActivity(intent)
-                    true
-                }
-                R.id.navigation_trophies -> {
-                    val intent = Intent(this,Retos::class.java)
-                    startActivity(intent)
-                    true
-                }
-                R.id.navigation_profile -> {
-                    val intent = Intent(this,Contactos::class.java)
-                    startActivity(intent)
-                    true
-                }
-                else -> false
-            }
 
+        binding.estadisticas.setOnClickListener {
+            val intent =  Intent(this,EstadisticasActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.seleccionarRecorrido.setOnClickListener {
+            val intent =  Intent(this,SeleccionarRecorrido::class.java)
+            startActivity(intent)
+        }
+
+        binding.botonRetos.setOnClickListener {
+            val intent =  Intent(this,RetosActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.contactos.setOnClickListener {
+            val intent =  Intent(this,Contactos::class.java)
+            startActivity(intent)
         }
 
         binding.flotante.setOnClickListener {
