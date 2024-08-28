5f50025127d3dc23551672cb28935b5f1690068e
Fabio Andrés Triana Rodriguez <124170936+Fabiotriana07@users.noreply.github.com>
2024-08-27 22:59:21 -0500

    Pantalla feed y navegacion entre login, registro y feed

--- HikeTrack/app/src/main/java/com/example/hiketrack/RegisterActivity.kt
+++ HikeTrack/app/src/main/java/com/example/hiketrack/RegisterActivity.kt
@@ -1,20 +1,41 @@
 package com.example.hiketrack
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.UnderlineSpan
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.hiketrack.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Subrayar el TextView de "Iniciar sesión" usando View Binding
+        val iniciarSesionText = SpannableString("Iniciar sesión")
+        iniciarSesionText.setSpan(UnderlineSpan(), 0, iniciarSesionText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.createAccountTextView.text = iniciarSesionText
+
+        // Configurar eventos de clic
+        binding.registerButton.setOnClickListener {
+            // Crear un Intent para ir a FeedActivity
+            val intent = Intent(this, FeedActivity::class.java)
+            startActivity(intent)
         }
+
+        binding.createAccountTextView.setOnClickListener {
+            // Crear un Intent para ir a LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
