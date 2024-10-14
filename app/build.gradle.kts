7bf22e51aadc622e7da4fa30852a7dac0a00fc7e
CryoWarrior <79274349+CryoWarrior@users.noreply.github.com>
2024-10-14 17:33:45 -0500

    resolve conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
 }
 
 android {
-    namespace = "com.example.hiketrack"
+    namespace = "com.example.vistastrabajo4"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.hiketrack"
+        applicationId = "com.example.vistastrabajo4"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -30,15 +31,15 @@ android {
             )
         }
     }
-    buildFeatures{
-        viewBinding = true
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
@@ -49,6 +50,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
