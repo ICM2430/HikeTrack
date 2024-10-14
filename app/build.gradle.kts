16279d2c924facd94715be561fc280aa3200585a
CryoWarrior <79274349+CryoWarrior@users.noreply.github.com>
2024-10-14 17:35:34 -0500

    Revert "Revert "corrigiendo el pull raro""

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,14 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
 }
 
 android {
-    namespace = "com.example.vistastrabajo4"
+    namespace = "com.example.hiketrack"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.vistastrabajo4"
+        applicationId = "com.example.hiketrack"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -27,15 +26,15 @@ android {
             )
         }
     }
+    buildFeatures{
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
         jvmTarget = "1.8"
-    }
-    buildFeatures {
-        viewBinding = true
     }
 }
 
@@ -46,8 +45,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // google maps
+    implementation("com.google.android.gms:play-services-maps:18.0.0")
 }
