3c5c310747cdb78585f7453c7bb635bdcb4de0c0
MariadlRosa-1224 <mariadlr1224@gmail.com>
2024-10-15 15:36:28 -0500

    funciones mapa

---
+++ app/src/main/java/com/example/hiketrack/model/MyLocation.kt
@@ -0,0 +1,18 @@
+package com.example.hiketrack.model
+
+import org.json.JSONObject
+import java.util.Date
+
+class MyLocation {
+    class MyLocation( val latitude: Double,
+                      val longitude: Double, val date : Date
+    ){
+        fun toJSON() : JSONObject {
+            val obj = JSONObject();
+            obj.put("latitude", latitude)
+            obj.put("longitude", longitude)
+            obj.put("date", date)
+            return obj
+        }
+    }
+}
