370e5c6e44ff6979fc60cfa19d30da276dbf5f52
CryoWarrior <diegoalejandrojararojas@gmail.com>
2024-10-15 23:25:36 -0500

    Implementacion configuracion de seguridad, elegir contacto

--- app/src/main/java/com/example/hiketrack/ConfiguracionCuentaActivity.kt
+++ app/src/main/java/com/example/hiketrack/ConfiguracionCuentaActivity.kt
@@ -1,20 +1,40 @@
 package com.example.hiketrack
 
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.hiketrack.databinding.ActivityConfiguracionCuentaBinding
+import com.example.hiketrack.databinding.ActivityConfiguracionSeguridadBinding
 
 class ConfiguracionCuentaActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityConfiguracionCuentaBinding
+    val galleryLauncher = registerForActivityResult(
+        ActivityResultContracts.GetContent(),
+        ActivityResultCallback {
+            loadImage(it!!)
+        }
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_configuracion_cuenta)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityConfiguracionCuentaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.picture.setOnClickListener{
+            galleryLauncher.launch("image/*")
         }
+
+    }
+    private fun loadImage(uri: Uri) {
+        val imageStream = getContentResolver().openInputStream(uri)
+        val bitmap = BitmapFactory.decodeStream(imageStream)
+        binding.picture.setImageBitmap(bitmap)
     }
 }
