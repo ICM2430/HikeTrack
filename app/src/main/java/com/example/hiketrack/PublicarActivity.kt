ad3ec5fd12fa9aa102b4f7466ae0be113fe4c86f
Jgon-2301 <juanfegq@gmail.com>
2024-10-15 23:54:26 -0500

    Añadiendo permisos en Publicar

--- app/src/main/java/com/example/hiketrack/PublicarActivity.kt
+++ app/src/main/java/com/example/hiketrack/PublicarActivity.kt
@@ -1,16 +1,39 @@
 package com.example.hiketrack
 
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.hiketrack.databinding.ActivityPublicarBinding
+import java.io.File
 
 class PublicarActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPublicarBinding
+    lateinit var uriCamera : Uri
+
+    val galleryLauncher = registerForActivityResult(
+        ActivityResultContracts.GetContent(),
+        ActivityResultCallback {
+            loadImage(it!!)
+        }
+    )
+
+    val getContentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture(),
+        ActivityResultCallback {
+            if(it){
+                loadImage(uriCamera)
+            }
+        }
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +50,23 @@ class PublicarActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.galleryButton.setOnClickListener{
+            galleryLauncher.launch("image/*")
+        }
+        binding.cameraButton.setOnClickListener{
+            val file = File(getFilesDir(), "picFromCamera");
+            uriCamera = FileProvider.getUriForFile(baseContext, baseContext.packageName +
+                    ".fileprovider", file)
+            getContentCamera.launch(uriCamera)
+        }
 
     }
+
+    private fun loadImage(uri: Uri) {
+        val imageStream = getContentResolver().openInputStream(uri)
+        val bitmap = BitmapFactory.decodeStream(imageStream)
+        binding.image.setImageBitmap(bitmap)
+    }
 }
+
+
